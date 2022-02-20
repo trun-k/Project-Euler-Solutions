a26a8f409c0075fa64e2b38775977d36e89798c6
Cascade398 <tarunk0205@gmail.com>
2022-02-20 09:27:55 +0530

    Solved Q6-7

--- euler_solutions.py
+++ euler_solutions.py
@@ -3,7 +3,7 @@ import euler_0001, euler_0002, euler_0003, euler_0004, euler_0005, euler_0006, e
 
 
 def main():
-    print(euler_0007.compute())
+    print(euler_0006.compute())
 
 
 if __name__ == '__main__':
